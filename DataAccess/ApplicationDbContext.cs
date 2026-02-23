a448d7bd1a093a0e94fa82341d22d4695a0b2ed0
RoKen1000 <rosskennedy993@outlook.com>
2026-02-23 14:48:10 +0000

    Create product entity, table, repository and components.

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace YumBlazor.DataAccess
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
