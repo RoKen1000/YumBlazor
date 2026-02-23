027d28f8f3c975a64c318e166afb3a030d8ad35f
RoKen1000 <rosskennedy993@outlook.com>
2026-02-23 14:48:10 +0000

    Create product entity, table, repository and components.

---
+++ YumBlazor.Abstractions/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Models.Entities;
+
+namespace YumBlazor.Abstractions.Repositories
+{
+    public interface IProductRepository
+    {
+        public Task<Product> CreateAsync(Product product);
+        public Task<Product> UpdateAsync(Product product);
+        public Task<bool> DeleteAsync(int id);
+        public Task<Product> GetByIdAsync(int id);
+        public Task<IEnumerable<Product>> GetAllAsync();
+    }
+}
